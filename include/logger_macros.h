8b3147e6121b7a8d2f5b9df6be68b43976498f2a
Isaac <isaaclatta73@gmail.com>
2025-04-01 16:02:29 -0700

    Added comments to logger macros

--- include/logger_macros.h
+++ include/logger_macros.h
@@ -23,13 +23,72 @@
         }                                                                \
     } while (0)
 
+/**
+ * @brief Logs a TRACE-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ * @note includes the line, function, and file at the end of the message
+ */
 #define TRACE(ctx, fmt, ...)  _LOG_INLINE(logger::level::Trace, ctx, fmt, ##__VA_ARGS__)
+
+/**
+ * @brief Logs a DEBUG-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ * @note includes the line, function, and file at the end of the message
+ */
 #define DEBUG(ctx, fmt, ...)  _LOG_INLINE(logger::level::Debug, ctx, fmt, ##__VA_ARGS__)
+
+/**
+ * @brief Logs a INFO-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ */
 #define INFO(ctx, fmt, ...)   _LOG_INLINE(logger::level::Info,  ctx, fmt, ##__VA_ARGS__)
+
+/**
+ * @brief Logs a WARN-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ * @note includes the line, function, and file at the end of the message
+ */
 #define WARN(ctx, fmt, ...)   _LOG_INLINE(logger::level::Warn,  ctx, fmt, ##__VA_ARGS__)
+
+/**
+ * @brief Logs a ERROR-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ * @note includes the line, function, and file at the end of the message
+ */
 #define ERROR(ctx, fmt, ...)  _LOG_INLINE(logger::level::Error, ctx, fmt, ##__VA_ARGS__)
+
+/**
+ * @brief Logs a STATUS-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ */
 #define STATUS(ctx, fmt, ...)  _LOG_INLINE(logger::level::Status, ctx, fmt, ##__VA_ARGS__)
 
+/**
+ * @brief Logs a FATAL-level message.
+ *
+ * @param ctx   the context of the log
+ * @param fmt   the printf-style format string.
+ * @param ...   additional arguments for the format string
+ * @note appends the pid and stack trace to the end of the log, will flush the logger and exit the program
+ */
 #define FATAL(ctx, fmt, ...) \
     do { \
         _LOG_INLINE(logger::level::Fatal, ctx, \
@@ -39,6 +98,12 @@
         exit(EXIT_FAILURE); \
     } while(0)
 
+/**
+ * @brief Logs a Session.
+ *
+ * @param entry a SessionEntry object (or rvalue) to be logged.
+ * @note will take ownership of the session entry
+ */
 #define LOG_SESSION(entry) \
     do { \
         auto entry_ptr = std::make_unique<logger::SessionEntry>(entry); \
